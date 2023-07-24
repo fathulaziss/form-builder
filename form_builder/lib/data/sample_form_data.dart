02ae78fbc8a052fbf55f5b0703c5c61f73b24df3
Muhamad Fathul Azis <azisfathl@gmail.com>
2023-07-24 14:05:20 +0700

    update

---
+++ form_builder/lib/data/sample_form_data.dart
@@ -0,0 +1,18 @@
+final Map<String, dynamic> sampleFormData = {
+  'fields': [
+    {
+      'key': 'inputKey',
+      'type': 'Input',
+      'label': 'Hi Group',
+      'placeholder': 'Hi Group flutter',
+      'required': true
+    },
+    {
+      'key': 'inputKey',
+      'type': 'Input',
+      'label': 'Initial Value',
+      'value': 'Hello',
+      'required': true
+    },
+  ],
+};
